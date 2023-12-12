02b33deafffac45a30ac44b8d564588a915c7fdb
Luca Heß <65811753+blockiyt@users.noreply.github.com>
2023-12-12 17:45:14 +0100

    update texts and added running plugins and selecting groups

--- actions.js
+++ actions.js
@@ -7,7 +7,7 @@ module.exports = function (self) {
 
 	self.setActionDefinitions({
 		quick_keys: {
-			name: 'Toggle Quick Keys',
+			name: 'Run Quick Keys',
 			options: [
 				{
 					type: 'textinput',
@@ -19,50 +19,61 @@ module.exports = function (self) {
 			],
 			callback: async (event) => {
 				const quickKey = await self.parseVariablesInString(event.options.quickKey)
-				const argsStr = await self.parseVariablesInString("\"flash\" 1")
 
-				const rawArgs = (argsStr + '').replace(/“/g, '"').replace(/”/g, '"').split(' ')
-
-				if (rawArgs.length) {
-					const args = []
-					for (let i = 0; i < rawArgs.length; i++) {
-						if (rawArgs[i].length == 0) continue
-						if (isNaN(rawArgs[i])) {
-							let str = rawArgs[i]
-							if (str.startsWith('"')) {
-								//a quoted string..
-								while (!rawArgs[i].endsWith('"')) {
-									i++
-									str += ' ' + rawArgs[i]
-								}
-							} else if(str.startsWith('{')) {
-								//Probably a JSON object
-								try {
-									args.push((JSON.parse(rawArgs[i])))
-								} catch (error) {
-									self.log('error', `not a JSON object ${rawArgs[i]}`)
-								}
-							}
-
-							args.push({
-								type: 's',
-								value: str.replace(/"/g, '').replace(/'/g, ''),
-							})
-						} else if (rawArgs[i].indexOf('.') > -1) {
-							args.push({
-								type: 'f',
-								value: parseFloat(rawArgs[i]),
-							})
-						} else {
-							args.push({
-								type: 'i',
-								value: parseInt(rawArgs[i]),
-							})
-						}
+				sendOscMessage("/13.13.1.9." + quickKey, [
+					{
+						type: 's',
+						value: 'flash',
+					},
+					{
+						type: 'i',
+						value: 1,
 					}
-
-					sendOscMessage("/13.13.1.9." + quickKey, args)
+				])
+			},
+		},
+		group: {
+			name: 'Select Group',
+			options: [
+				{
+					type: 'textinput',
+					label: 'Group Number',
+					id: 'group',
+					default: '1',
+					useVariables: true,
 				}
+			],
+			callback: async (event) => {
+				const group = await self.parseVariablesInString(event.options.group)
+
+				sendOscMessage("/cmd", [
+					{
+						type: 's',
+						value: 'SelectFixtures Group ' + group,
+					}
+				])
+			},
+		},
+		plugin: {
+			name: 'Call Plugins',
+			options: [
+				{
+					type: 'textinput',
+					label: 'Plugin Number',
+					id: 'plugin',
+					default: '1',
+					useVariables: true,
+				}
+			],
+			callback: async (event) => {
+				const plugin = await self.parseVariablesInString(event.options.plugin)
+
+				sendOscMessage("/cmd", [
+					{
+						type: 's',
+						value: 'Call Plugin ' + plugin,
+					}
+				])
 			},
 		},
 	})
