9a8b30ae6e44511abe0c5896e2a07f19aaed5276
Luca Heß <65811753+blockiyt@users.noreply.github.com>
2024-01-30 16:36:45 +0100

    fixed quickey not being disabled, by that you have to add ad push and release action to your button

--- actions.js
+++ actions.js
@@ -21,22 +21,52 @@ module.exports = function (self) {
 					id: 'quickey',
 					default: 1,
 				},
+				{
+					id: 'button_state',
+					type: 'dropdown',
+					label: 'Button State',
+					default: 'press',
+					choices: [
+						{
+							id: 'press',
+							label: 'Press',
+						},
+						{
+							id: 'release',
+							label: 'Release',
+						},
+					],
+				},
 			],
 			callback: async (event) => {
+				const button_state = await self.parseVariablesInString(event.options.button_state)
 				const quickey = await self.parseVariablesInString(event.options.quickey)
 
 				self.log('debug', `Quickey ${quickey}`)
 
-				sendOscMessage('/13.13.1.9.' + quickey, [
-					{
-						type: 's',
-						value: 'flash',
-					},
-					{
-						type: 'i',
-						value: 1,
-					},
-				])
+				if (button_state === 'press') {
+					sendOscMessage('/13.13.1.9.' + quickey, [
+						{
+							type: 's',
+							value: 'Go+',
+						},
+						{
+							type: 'i',
+							value: 1,
+						},
+					])
+				} else {
+					sendOscMessage('/13.13.1.9.' + quickey, [
+						{
+							type: 's',
+							value: 'Go+',
+						},
+						{
+							type: 'i',
+							value: 0,
+						},
+					])
+				}
 			},
 		},
 		group: {
