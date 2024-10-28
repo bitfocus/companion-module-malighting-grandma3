0ff3c06529925a3440b413a21e40786ab4d85634
Luca Heß <65811753+blockiyt@users.noreply.github.com>
2024-10-28 16:53:51 +0100

    added upgrade script for exec_button conversion from string to integer

--- src/upgrades.ts
+++ src/upgrades.ts
@@ -1,7 +1,37 @@
-import type { CompanionStaticUpgradeScript } from '@companion-module/base'
+import type {
+	CompanionStaticUpgradeProps,
+	CompanionStaticUpgradeResult,
+	CompanionStaticUpgradeScript,
+	CompanionUpgradeContext,
+} from '@companion-module/base'
 import type { ModuleConfig } from './config.js'
 
+function updateExecButtonPageFromDropdownToInteger(
+	_context: CompanionUpgradeContext<ModuleConfig>,
+	props: CompanionStaticUpgradeProps<ModuleConfig>,
+): CompanionStaticUpgradeResult<ModuleConfig> {
+	const result: CompanionStaticUpgradeResult<ModuleConfig> = {
+		updatedActions: [],
+		updatedConfig: null,
+		updatedFeedbacks: [],
+	}
+
+	for (const action of props.actions) {
+		if (
+			action.actionId === 'exec_button' &&
+			typeof action.options.page === 'string' &&
+			!isNaN(parseInt(action.options.page))
+		) {
+			action.options.page = parseInt(action.options.page)
+			result.updatedActions.push(action)
+		}
+	}
+
+	return result
+}
+
 export const UpgradeScripts: CompanionStaticUpgradeScript<ModuleConfig>[] = [
+	updateExecButtonPageFromDropdownToInteger,
 	/*
 	 * Place your upgrade scripts here
 	 * Remember that once it has been added it cannot be removed!
